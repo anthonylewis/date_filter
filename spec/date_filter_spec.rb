3b163d51253731c25662d2db3171a0d054b0bc8e
Anthony Lewis <me@anthonylewis.com>
2011-04-25 21:38:44 -0500

    Getting read for RSpec

---
+++ spec/date_filter_spec.rb
@@ -0,0 +1,11 @@
+
+# it should add method newest
+# it should add method oldest
+# newest should return nil for empty table
+# oldest should retrun nil for empty table
+# newest should equal oldest for single record table
+# newest should return last record
+# oldest should return first record
+#
+# What about records without created_at ?
+#

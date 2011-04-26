5b866ae7fcffe21e0644c24b36785eb2d9a36965
Anthony Lewis <me@anthonylewis.com>
2011-04-26 16:01:30 -0500

    Adding test database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,13 @@ require 'bundler'
 Bundler.setup
 Bundler.require
 
-RSpec.configure do |config|
-end
+# create an in-memory sqlite3 database
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => ":memory:"
+)
+
+# load the schema without output
+ActiveRecord::Migration.verbose = false
+load File.expand_path('../db/schema.rb', __FILE__)
 
